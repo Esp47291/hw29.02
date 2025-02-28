c4ca6226ed70b6319c37bae87dbd23b3b741c6a9
Alekseev.D <gipno262@gmail.com>
2025-02-28 12:37:29 +0300

    hw19.02

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ class Task1 {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Некорректное значение clientOS");
         }
     }
 }
@@ -17,14 +19,23 @@ class Task2 {
         int clientOS = 0; // 0 - iOS, 1 - Android
         int clientDeviceYear = 2015;
 
+        // Проверка корректности clientOS
+        if (clientOS != 0 && clientOS != 1) {
+            System.out.println("Некорректное значение clientOS");
+            return;
+        }
+
+        // Условное выражение соответствует требованиям задания
         String baseMessage = clientOS == 0 ?
                 "Установите версию приложения для iOS по ссылке" :
                 "Установите версию приложения для Android по ссылке";
 
+        // Вывод зависит от clientDeviceYear, без оператора else (тернарный оператор)
         String finalMessage = clientDeviceYear < 2015 ?
                 baseMessage.replace("версию", "облегченную версию") :
                 baseMessage;
 
+        // Имена переменных отражают их содержимое, двойная вложенность отсутствует
         System.out.println(finalMessage);
     }
 }
@@ -34,16 +45,24 @@ class Task3 {
     public static void main(String[] args) {
         int year = 2021;
 
-        if (year < 1584) {
+        // Проверка валидности года
+        if (year <= 0) {
+            System.out.println("Год должен быть положительным");
+            return;
+        } else if (year < 1584) {
             System.out.println("Год должен быть больше 1584");
             return;
         }
 
+        // Условное выражение для високосного года соответствует требованиям
         boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        // Вывод зависит от переменной, без else (тернарный оператор)
         String message = isLeap ?
                 year + " год является високосным" :
                 year + " год не является високосным";
 
+        // Имена переменных отражают содержимое, двойная вложенность отсутствует
         System.out.println(message);
     }
 }
@@ -52,6 +71,12 @@ class Task3 {
 class Task4 {
     public static void main(String[] args) {
         int deliveryDistance = 95;
+
+        // Проверка корректности расстояния
+        if (deliveryDistance < 0) {
+            System.out.println("Расстояние не может быть отрицательным");
+            return;
+        }
 
         int days;
         if (deliveryDistance <= 20) {
@@ -74,12 +99,13 @@ class Task5 {
     public static void main(String[] args) {
         int monthNumber = 12;
 
+        // Проверка корректности номера месяца
         if (monthNumber > 12 || monthNumber < 1) {
             System.out.println("Номер месяца должен быть от 1 до 12");
             return;
         }
 
-        String season;
+        String season = "";
         switch (monthNumber) {
             case 12:
             case 1:
@@ -100,9 +126,6 @@ class Task5 {
             case 10:
             case 11:
                 season = "осень";
-                break;
-            default:
-                season = "неизвестный сезон";
                 break;
         }
 
